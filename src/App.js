730302e52062c41d39f9d892eb6465b5b059dff3
thatsrohitnaik <rohit24naik@gmail.com>
2021-10-02 00:17:22 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React, { useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
 import StoreContext from './Context/';
 import Weather from './Pages/Weather';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import './style.css';
 
 function App() {
   return (
-    <div>
-      <Weather />
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Container fixed>
+        <Box>
+          <Weather />
+        </Box>
+      </Container>
+    </React.Fragment>
   );
 }
 
