12f1e1f4be094ffe5897158ce276804529e22181
thatsrohitnaik <rohit24naik@gmail.com>
2021-10-04 01:11:09 +0530

    2 files modified

--- src/Components/Loading/index.js
+++ src/Components/Loading/index.js
@@ -1,5 +1,29 @@
 import React from 'react';
+import Slider from 'react-slick';
+import { slideSettings } from '../../settings';
+import Skeleton from '@mui/material/Skeleton';
 
 export default function Loading() {
-  return <div>Loading</div>;
+  return (
+    <div>
+      <br />
+      <Slider {...slideSettings}>
+        <div>
+          <Skeleton variant="rectangular" width={'95%'} height={118} />
+          <Skeleton width="95%" />
+          <Skeleton width="60%" />
+        </div>
+        <div>
+          <Skeleton variant="rectangular" width={'95%'} height={118} />
+          <Skeleton width="95%" />
+          <Skeleton width="60%" />
+        </div>
+        <div>
+          <Skeleton variant="rectangular" width={'95%'} height={118} />
+          <Skeleton width="95%" />
+          <Skeleton width="60%" />
+        </div>
+      </Slider>
+    </div>
+  );
 }
