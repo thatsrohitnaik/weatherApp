575080e31b6e04139706e8f27633ee3471070037
thatsrohitnaik <rohit24naik@gmail.com>
2021-10-04 11:54:47 +0530

    1 file modified

--- src/settings.js
+++ src/settings.js
@@ -1,19 +1,18 @@
 export const slideSettings = {
   arrows: true,
-  accessibility: true,
   infinite: false,
-  speed: 500,
+  speed: 1000,
   slidesToShow: 3,
   slidesToScroll: 3,
   initialSlide: 0,
-  focusOnSelect: true,
-  useTransform: true,
   responsive: [
     {
       breakpoint: 1024,
       settings: {
         slidesToShow: 3,
         slidesToScroll: 3,
+        speed: 500,
+        infinite: false,
       },
     },
     {
